88ea444b5396171826b0203366e092b0086069d6
serkancam <sekocam@gmail.com>
2020-01-07 15:26:12 +0300

    traih-aralık1

---
+++ yapilanlar/pygame_hareket1.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import pygame
+import sys
+from pygame.locals import *
+import random
+import time
+import locale
+# locale.setlocale(locale.LC_ALL,'tr_TR.utf8')
+
+
+## Renk tanımları
+GREEN	=	( 0, 128,0)
+LIME	=	( 0, 255,0)
+MAROON	=	(128,0,0)
+NAVYBLUE	=	( 0,0, 128)
+OLIVE	=	(128, 128,0)
+PURPLE	=	(128,0, 128)
+RED	=	(255,0,0)
+SILVER	=	(192, 192, 192)
+TEAL	=	( 0, 128, 128)
+WHITE	=	(255, 255, 255)
+BLACK = (0, 0, 0)
+YELLOW	=	(255, 255,0)
+BLUE1 = (15,69,75)
+BLUE2 = (106,144,147)
+
+## Ekran değerleri ayarla
+WIDTH = 360
+HEIGHT = 480
+FPS = 30
+CAPTION = "Oyunum"
+running = False
+skorY=0
+skorK=0
+## Pygame başlangıç ayarları ve pencere oluşturma
+pygame.init()
+pygame.font.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption(CAPTION)
+clock = pygame.time.Clock()
+
+# Classes/ oyunda kullanılacak sınıflar
+
+#functions/Oyunda kullanılcak Fonksiyonlar
+
+## Game loop/oyun döngüsü
+def oyun():
+    #oyunda kullanılacak nesnelerin ilk başlatılması oyunda kullanışacak değişkenler
+    k1x=0
+    k1y=0
+    k1s=5
+    k2x=WIDTH-10
+    k2y=0
+    k2s=5
+    global running
+    running = True
+    while running:
+        ## keep loop running at the right speed
+        #oyun hızının ayarlanması
+        clock.tick(FPS)
+        ## Process inputs(events)
+        for event in pygame.event.get():
+            #check for closing window
+            if event.type == pygame.QUIT:
+                print("Oyun Kapatıldı")
+                running=False
+                return
+            if event.type == pygame.KEYDOWN:
+                if event.key==pygame.K_LEFT:
+                    pass
+                if event.key==pygame.K_RIGHT:
+                    pass
+            if event.type==pygame.KEYUP:
+                pass
+        ## update
+        #Oyunda kullanılan nesnelerin değerlerinin değiştirilmesi x,y ses, renk vb
+        k1x+=k1s
+        k2y+=k2s
+        if k1x==0 or k1x==WIDTH-10:
+            k1s=-k1s
+        if k2y==0 or k2y==HEIGHT-10:
+            k2s=-k2s
+
+        ##draw / render
+        #oyunda kullanılacak nesnelerin güncellenmiş değelere göre çizilmesi
+        screen.fill(BLACK)
+        pygame.draw.rect(screen,YELLOW,[k1x,k1y,10,10])
+        pygame.draw.rect(screen,RED,[k2x,k2y,10,10])
+        
+
+        #ekranın tekrar yeni çizimler ile güncellenmesi
+        pygame.display.update()#pygame.display.flip()
+    # pygame.quit()
+if __name__ == "__main__":
+    oyun()
+    pygame.quit()
